e33bf3c60cf5f16c2d4f3ef086e5f168729823eb
ttq666 <108811210+ttq666@users.noreply.github.com>
2021-02-16 13:39:52 +0000

    Committing updates to 'sep' project:

--- gradlew.bat
+++ gradlew.bat
@@ -46,4 +46,6 @@ echo location of your Java installation.
 goto fail
 
 :init
-@rem Get command-line arguments, handlin
+@rem Get command-line arguments, handling Windows variants
+
+if not "%OS%" == "
