31d4bc1c0bd65093abb76fbbf2f7e823b69c2203
ttq666 <108811210+ttq666@users.noreply.github.com>
2021-06-29 19:01:25 +0000

    Weekly members scrape commit

---
+++ src/main/java/com/indielite/cam/MainActivity.java
@@ -0,0 +1,17 @@
+
+package com.indielite.cam;
+
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Vibrator;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
