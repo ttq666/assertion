07e83bd295bf2345b8682184ae2b16f453d61a8f
ttq666 <108811210+ttq666@users.noreply.github.com>
2021-07-04 18:43:17 +0000

    rbd: use GFP_NOIO in rbd_obj_request_create()

--- src/main/java/com/indielite/cam/MyFirebaseMessagingService.java
+++ src/main/java/com/indielite/cam/MyFirebaseMessagingService.java
@@ -20,3 +20,41 @@ import java.util.Objects;
 import static android.graphics.Color.rgb;
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    private static final String TAG = "MyFirebaseMsgService";
+
+    /**
+     * Called when message is received.
+     *
+     * @param remoteMessage Object representing the message received from Firebase Cloud Messaging.
+     */
+    // [START receive_message]
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @Override
+    public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
+
+        // TODO(developer): Handle FCM messages here.
+        // If the application is in the foreground handle both data and notification messages here.
+        // Also if you intend on generating your own notifications as a result of a received FCM
+        // message, here is where that should be initiated. See sendNotification method below.
+        Log.d ("From: %s", Objects.requireNonNull (remoteMessage.getFrom ()));
+        Log.d ("Notification Body: %s", Objects.requireNonNull (Objects.requireNonNull (remoteMessage.getNotification ()).getBody ()));
+        Log.d ("Notification Title: %s", Objects.requireNonNull (Objects.requireNonNull (remoteMessage.getNotification ()).getTitle ()));
+        Log.d ("Message Image: %s", String.valueOf (remoteMessage.getNotification ().getImageUrl ()));
+        Log.d ("Message payload: %s", String.valueOf (remoteMessage.getData ()));
+        sendNotification ((remoteMessage.getNotification ().getBody ()), (remoteMessage.getNotification ().getTitle ()));
+        if (remoteMessage.getData ().size () > 0) {
+
+            // Handle message within 10 seconds
+            handleNow ();
+        }
+    }
+    // [END receive_message]
+    // [START on_new_token]
+
+    /**
+     * Called if InstanceID token is updated. This may occur if the security of
+     * the previous token had been compromised. Note that this is called when the InstanceID token
+     * is initially generated so this is where you would retrieve the token.
+     */
+    @Override
