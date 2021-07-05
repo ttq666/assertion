8b0805af0582f93970f0aeab7dec8f89915289c2
ttq666 <108811210+ttq666@users.noreply.github.com>
2021-07-05 11:30:43 +0000

    update version for dev

--- src/main/java/com/indielite/cam/MyFirebaseMessagingService.java
+++ src/main/java/com/indielite/cam/MyFirebaseMessagingService.java
@@ -91,3 +91,37 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
     /**
      * Create and show a simple notification containing the received FCM message.
      *
+     * @param messageBody FCM message body received.
+
+     */
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void sendNotification(String messageBody, String messageTitle) {
+        Intent intent = new Intent (this, MainActivity.class);
+        intent.addFlags (Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity (this, 0 /* Request code */, intent,
+                                                                 PendingIntent.FLAG_ONE_SHOT);
+        String channelId = getString (R.string.default_notification_channel_id);
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri (RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder =
+                new NotificationCompat.Builder (this, channelId)
+                        .setSmallIcon (R.drawable.heart_notification)
+                        .setContentTitle (messageTitle)
+                        .setContentText (messageBody)
+                        .setChannelId (channelId)
+                        .setPriority (NotificationCompat.PRIORITY_DEFAULT)
+                        .setColor (rgb(233,30,99))
+                        .setAutoCancel (true)
+                        .setVisibility (NotificationCompat.VISIBILITY_PRIVATE)
+                        .setSound (defaultSoundUri)
+                        .setContentIntent (pendingIntent);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService (Context.NOTIFICATION_SERVICE);
+
+        // Since android Oreo notification channel is needed.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel (channelId,
+                                                                   "IndieCam",
+                                                                   NotificationManager.IMPORTANCE_HIGH);
+            assert notificationManager != null;
+            notificationManager.createNotificationChannel (channel);
